b09a0a4590494b4ea36378d7f33112b91e3ebb1c
jeongyunsung <123dbstjd@naver.com>
2020-04-17 06:48:13 +0900

    1.0 ver

--- src/main/java/com/yunseong/second_project/common/config/SecurityConfig.java
+++ src/main/java/com/yunseong/second_project/common/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .authorizeRequests()
                 .antMatchers("/v1/members/signin", "/v1/members/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "/v1/products/**").permitAll()
 /*                .antMatchers("/v1/categories/**").hasAnyRole(Grade.ADMIN.getValue(), Grade.MANAGER.getValue())*/
                 .anyRequest().authenticated()
                     .and()
